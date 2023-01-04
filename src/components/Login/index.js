12a1b466dbb757701deec67d9cdd9b76fda50713
c4az6 <1762458611@qq.com>
2023-01-05 05:48:51 +0800

    引入axios发送请求

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+
+import axios from 'axios'
+
 import './index.scss';
 import Logo from './Logo'
 import Form from './Form'
-
 export default class Login extends Component {
+
+  getCourseData() {
+    axios('//localhost:3000/get_course').then(res => {
+      console.log("response: ", res);
+    }).catch(err => {
+      console.log('response error: ', err);
+    })
+  }
+
+  componentDidMount() {
+    this.getCourseData();
+  }
+
   render() {
     return (
       <div className="login-container">
