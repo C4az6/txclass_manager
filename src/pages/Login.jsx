59e5fd47f5846cc5595efa86f9a18b7879a9edda
c4az6 <1762458611@qq.com>
2023-01-05 08:43:56 +0800

    优化登录页样式，增加背景图

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Login from 'components/Login'
+import './login.scss'
 export default class LoginPage extends Component {
   constructor(props) {
     super(props)
@@ -12,7 +13,7 @@ export default class LoginPage extends Component {
     // 只要注册过路由的组件都有history属性，可以传递给子组件
     const { history } = this.props;
     return (
-      <div>
+      <div className="login-page-container" >
         <Login history={history}></Login>
       </div>
     )
